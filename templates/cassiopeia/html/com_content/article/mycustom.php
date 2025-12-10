599b9a3f4557604d35b389f2b9b42e14ef1bf3a9
Nirajan Poudel <nirajan.poudel@nitv.com.np>
2025-12-10 15:24:16 +0545

    add changes

--- templates/cassiopeia/html/com_content/article/mycustom.php
+++ templates/cassiopeia/html/com_content/article/mycustom.php
@@ -11,24 +11,26 @@
     // Loop through items
     $a = 1;
     foreach ($rss->channel->item as $i=>$item) {
-        $a++;
+        $doiLink = "";
+        $desc = html_entity_decode((string)$item->description);
+        $title = trim((string)$item->title);
+
+        if (preg_match('/https:\/\/doi\.org\/[^\s<"]+/', $desc, $match)) {
+            $doiLink = $match[0];
+        }
+        $desc = preg_replace('/ORCID:\s*https?:\/\/orcid\.org\/[^\s<]+/i', '', $desc);
+
         echo "<div style='margin-bottom:10px;'>";
-        echo "<h5><a href='{$item->link}' target='_blank'>{$item->title}</a></h5>";
+        if ($doiLink != "") {
+            echo "<h5><a href='{$doiLink}' target='_blank'>{$title}</a></h5>";
+        } else {
+            echo "<h5>{$title}</h5>";
+        }
 
         // Description
         if (!empty($item->description)) {
-            $raw = html_entity_decode($item->description);
 
-        $desc = preg_replace_callback(
-                '/<(https?:\/\/[^>]+)>/',
-                    function ($m) {
-                        $url = $m[1];
-                        return '<a href="' . $url . '" target="_blank">' . $url . '</a>';
-                },
-                $raw
-            );
-
-        echo "<p>$desc</p>";
+        echo "<p>($desc)</p>";
 
         }
 
